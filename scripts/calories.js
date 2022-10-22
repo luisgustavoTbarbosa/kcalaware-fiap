9931bb330a2155ae00fd931a5ceacc5e66e36ac7
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-10-22 08:02:35 -0300

    implement api

--- scripts/calories.js
+++ scripts/calories.js
@@ -1,6 +1,11 @@
+import api from './api.js';
+
+console.log(api)
+
 const buttonsAddFood = document.querySelectorAll('.button-add');
 const addFoodModal = document.querySelector('.addFoodModal');
 const buttonCloseModal = document.querySelector('.range-closeModal');
+const inputSearchFood = document.querySelector('.searchFood');
 
 buttonsAddFood.forEach(button => {
     button.addEventListener('click', openModalAddFood);
@@ -13,5 +18,36 @@ function openModalAddFood() {
 }
 
 function closeModalFood() {
-    addFoodModal.classList.remove('open')
+    addFoodModal.classList.remove('open');
+}
+
+
+inputSearchFood.addEventListener('keyup', getSearchFood)
+function getSearchFood() {
+    const listFoodsDom = document.querySelector('.list-foods ul');
+    const foodSearch = api.filter(food => food.description.toLowerCase().startsWith(inputSearchFood.value));
+    console.log(foodSearch)
+    listFoodsDom.innerHTML = ` `
+
+    if(inputSearchFood.value === '') {
+        listFoodsDom.innerHTML = `<li class="item-loading">
+                                    <div class="checkbox"></div>
+                                    <div>
+                                        <p>xxxxx kcal</p>
+                                        <p class="nameFood">xxxxx</p>
+                                        <p>1 peito (172g)</p>
+                                    </div>
+                                </li>`
+    } else {
+        foodSearch.forEach(food => {
+        listFoodsDom.innerHTML += `<li>
+                                        <div class="checkbox"></div>
+                                        <div>
+                                            <p>${String(food.attributes.energy.kcal).substr(1, 4)} kcal</p>
+                                            <p class="nameFood">${food.description}</p>
+                                            <p>100g</p>
+                                        </div>
+                                    </li>`
+    })
+    }
 }
